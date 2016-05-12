2945674353704cbfb594b423ae759119d044d77e
konwboj <mcmodspl@gmail.com>
2016-05-12 19:57:48 +0200

    Spawning system enhancement

--- src/tumbleweed/common/CommonEventHandler.java
+++ src/tumbleweed/common/CommonEventHandler.java
@@ -57,9 +57,7 @@ public class CommonEventHandler
 									++i;
 
 									if (!flag && world.getWorldBorder().contains(chunkcoordintpair))
-									{
 										eligibleChunksForSpawning.add(chunkcoordintpair);
-									}
 								}
 							}
 						}
@@ -70,49 +68,52 @@ public class CommonEventHandler
 				int j4 = world.countEntities(EntityTumbleweed.class);
 				int k4 = 35 * i / MOB_COUNT_DIV;
 
-				if (j4 <= k4)
+				for (ChunkCoordIntPair chunkcoordintpair : eligibleChunksForSpawning)
 				{
-					for (ChunkCoordIntPair chunkcoordintpair1 : eligibleChunksForSpawning)
-					{
-						if (world.rand.nextFloat() < 0.5f)
-						{
-							BlockPos blockpos = getRandomChunkPosition(world, chunkcoordintpair1.chunkXPos, chunkcoordintpair1.chunkZPos);
-							BlockPos deadBush = null;
-							int r = 4;
+					if (j4 > k4)
+						break;
 
-							for (int x = -r; x < r; x++)
+					if (world.rand.nextFloat() < 0.4f)
+					{
+						BlockPos blockpos = getRandomChunkPosition(world, chunkcoordintpair.chunkXPos, chunkcoordintpair.chunkZPos);
+						BlockPos deadBush = null;
+						int r = 4;
+
+						for (int x = -r; x < r; x++)
+						{
+							for (int y = -r; y < r; y++)
 							{
-								for (int y = -r; y < r; y++)
+								for (int z = -r; z < r; z++)
 								{
-									for (int z = -r; z < r; z++)
+									BlockPos check = new BlockPos(blockpos.getX() + x, blockpos.getY() + y, blockpos.getZ() + z);
+									Block block = world.getBlockState(check).getBlock();
+									if (block == Blocks.deadbush && world.getLight(check) > 8)
 									{
-										BlockPos check = new BlockPos(blockpos.getX() + x, blockpos.getY() + y, blockpos.getZ() + z);
-										Block block = world.getBlockState(check).getBlock();
-										if (block == Blocks.deadbush && world.getLight(check) > 8)
-										{
-											deadBush = check;
-											break;
-										}
+										deadBush = check;
+										break;
 									}
 								}
 							}
+						}
 
-							if (deadBush != null)
+						if (deadBush != null)
+						{
+							int x = deadBush.getX();
+							int y = deadBush.getY();
+							int z = deadBush.getZ();
+
+							if (!world.isAnyPlayerWithinRangeAt((double) x, (double) y, (double) z, 24.0D) && spawnPoint.distanceSq((double) x, (double) y, (double) z) >= 24.0 * 24.0)
 							{
-								int x = deadBush.getX();
-								int y = deadBush.getY();
-								int z = deadBush.getZ();
-
-								if (!world.isAnyPlayerWithinRangeAt((double) x, (double) y, (double) z, 24.0D) && spawnPoint.distanceSq((double) x, (double) y, (double) z) >= 24.0 * 24.0)
+								BiomeGenBase biome = world.getBiomeGenForCoords(deadBush);
+								if (biome.temperature > 1.8f && biome.rainfall == 0f)
 								{
-									BiomeGenBase biome = world.getBiomeGenForCoords(deadBush);
-									if (biome.temperature > 1.8f && biome.rainfall == 0f)
-									{
-										EntityTumbleweed entity = new EntityTumbleweed(world);
-										entity.setLocationAndAngles((double) x + 0.5, (double) y + 0.5, (double) z + 0.5, 0.0F, 0.0F);
+									EntityTumbleweed entity = new EntityTumbleweed(world);
+									entity.setLocationAndAngles((double) x + 0.5, (double) y + 0.5, (double) z + 0.5, 0.0F, 0.0F);
 
-										if (entity.isNotColliding())
-											world.spawnEntityInWorld(entity);
+									if (entity.isNotColliding())
+									{
+										j4++;
+										world.spawnEntityInWorld(entity);
 									}
 								}
 							}
@@ -134,6 +135,7 @@ public class CommonEventHandler
 
 			this.ticks++;
 		}
+
 	}
 
 	@SubscribeEvent
