28e8af678c96132fac2df63103e33d349480d79c
konwboy <mcmodspl@gmail.com>
2017-06-22 23:44:24 +0200

    Use lombok, support metadata in config

--- src/main/java/net/konwboy/tumbleweed/common/Config.java
+++ src/main/java/net/konwboy/tumbleweed/common/Config.java
@@ -2,6 +2,7 @@ package net.konwboy.tumbleweed.common;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import lombok.Data;
 import net.konwboy.tumbleweed.Tumbleweed;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -12,6 +13,7 @@ import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
 import org.apache.logging.log4j.Level;
 
+import javax.annotation.Nullable;
 import java.io.File;
 import java.util.List;
 import java.util.Set;
@@ -24,7 +26,7 @@ public class Config {
 	private static int totalWeight;
 	private static double spawnChance;
 	private static Set<ResourceLocation> biomeWhitelist = Sets.newHashSet();
-	private static Set<Block> spawningBlocks = Sets.newHashSet();
+	private static Set<Metadata> spawningBlocks = Sets.newHashSet();
 
 	private static final double DEFAULT_CHANCE = 0.4;
 	private static final String[] DEFAULT_DROPS = { "3 minecraft:bone", "3 minecraft:deadbush", "3 minecraft:string", "3 minecraft:feather", "3 minecraft:wheat", "3 minecraft:stick", "3 minecraft:reeds", "2 minecraft:melon_seeds", "2 minecraft:pumpkin_seeds", "2 minecraft:gold_nugget", "1 minecraft:name_tag", "1 minecraft:saddle", "1 minecraft:emerald", "1 minecraft:diamond", "1 minecraft:iron_ingot", "1 minecraft:gold_ingot" };
@@ -52,7 +54,14 @@ public class Config {
 					continue;
 
 				double weight = Double.parseDouble(itemData[0]);
-				weightedItems.add(new WeightedItem(weight, itemData[1]));
+				Metadata meta = getMetadata(itemData[1]);
+
+				if (meta == null || !Item.REGISTRY.containsKey(meta.id)) {
+					Tumbleweed.logger.log(Level.WARN, "Item {} is invalid.", itemData[1]);
+					continue;
+				}
+
+				weightedItems.add(new WeightedItem(getMetadata(itemData[1]), weight));
 				totalWeight += weight;
 			}
 		}
@@ -76,11 +85,12 @@ public class Config {
 		blockConfig.setComment("The blocks in which tumbleweeds can spawn in.");
 		if (blockConfig.isList())
 			for (String entry : blockConfig.getStringList()) {
-				Block block = Block.getBlockFromName(entry);
-				if (block != null)
-					spawningBlocks.add(block);
-				else
-					Tumbleweed.logger.log(Level.WARN, "Block {} doesn't exist.", entry);
+				Metadata meta = getMetadata(entry);
+				if (meta == null || !Block.REGISTRY.containsKey(meta.id)) {
+					Tumbleweed.logger.log(Level.WARN, "Block {} is invalid.", entry);
+					continue;
+				}
+				spawningBlocks.add(meta);
 			}
 
 		if (config.hasChanged())
@@ -95,14 +105,9 @@ public class Config {
 			countedWeight += weightedItem.getWeight();
 
 			if (countedWeight >= randomWeight) {
-				String[] parts = weightedItem.getId().split(":");
-				Item item = Item.REGISTRY.getObject(new ResourceLocation(parts[0] + ":" + parts[1]));
-				int meta = 0;
-				if (parts.length >= 3)
-					meta = Integer.parseInt(parts[2]);
-
+				Item item = Item.REGISTRY.getObject(weightedItem.meta.id);
 				if (item != null)
-					return new ItemStack(item, 1, meta);
+					return new ItemStack(item, 1, weightedItem.meta.meta);
 			}
 		}
 
@@ -117,27 +122,35 @@ public class Config {
 		return biomeWhitelist;
 	}
 
-	public static Set<Block> getSpawningBlocks() {
+	public static Set<Metadata> getSpawningBlocks() {
 		return spawningBlocks;
 	}
 
+	@Nullable
+	private static Metadata getMetadata(String id) {
+		String[] split = id.split(":");
+		if (split.length < 2)
+			return null;
+
+		int meta = 0;
+		if (split.length >= 3)
+			meta = Integer.parseInt(split[2]);
+
+		return new Metadata(new ResourceLocation(split[0], split[1]), meta);
+	}
+
+	@Data
+	public static class Metadata {
+
+		private final ResourceLocation id;
+		private final int meta;
+	}
+
+	@Data
 	private static class WeightedItem {
 
+		private final Metadata meta;
 		private final double weight;
-		private final String id;
-
-		private WeightedItem(double weight, String id) {
-			this.weight = weight;
-			this.id = id;
-		}
-
-		private double getWeight() {
-			return weight;
-		}
-
-		private String getId() {
-			return id;
-		}
 	}
 
 }
